c46beac54f0948428aba078f8335d9e4fef91b59
Mitchell Mason <mmaso14@tigers.lsu.edu>
2015-03-11 15:06:01 -0500

    Added help from old projects in comments

---
+++ experiments/UDPVidStream/GLTest/GLTest.hpp
@@ -0,0 +1,14 @@
+//
+//  GLTest.h
+//  
+//
+//  Created by Mitchell Mason on 3/4/15.
+//
+//
+
+#ifndef ____GLTest__
+#define ____GLTest__
+
+#include <stdio.h>
+
+#endif /* defined(____GLTest__) */
